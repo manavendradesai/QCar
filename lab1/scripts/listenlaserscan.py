1f4adb49d4d407e1c6790f8a9a97f5b06537f9e7
Manavendra Desai <manavendradesai@wayne.edu>
2022-06-20 12:20:05 -0400

    Created custom message for processed laserscan data.

--- lab1/scripts/listenlaserscan.py
+++ lab1/scripts/listenlaserscan.py
@@ -4,6 +4,7 @@ import rospy
 import numpy as np
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float64
+from lab1.msg import scan_range
 
 #Replace NaNs and Infs with this
 minusnone = -1;
@@ -12,20 +13,21 @@ def callback(ranges):
 	#Filter NaNs and Inf
 	mapping = {np.nan:0,np.inf:0} 
 	filteredranges = [mapping.get(x,x) for x in ranges.ranges]	
-	#rospy.loginfo(rospy.get_caller_id() + "Furthest point %s",max(filteredranges))
 	
 	#Publish max min range over two separate topics
-	pub_close = rospy.Publisher('closest_point',Float64,queue_size=10)
+	pub_close = rospy.Publisher('closest_point',Float64,queue_size=1)
 	pub_far = rospy.Publisher('farthest_point',Float64,queue_size=1)
+	pub_again = rospy.Publisher('scan_range',scan_range,queue_size=1)
 
 	minpoint = sorted(filteredranges)[0] if minusnone not in filteredranges else sorted(filteredranges)[1] 
-	maxpoint = max(filteredranges) 
- 	#rospy.loginfo("Closest point: %s",minpoint)
-	#rospy.loginfo("Furthest point: %s", maxpoint)	
+	maxpoint = max(filteredranges)
 	print('Furthest point: ', maxpoint, 'Closest point: ', minpoint)
+
 	pub_close.publish(minpoint)
 	pub_far.publish(maxpoint)
 
+	#Publish laserscan header, min and maxpoint through a message on another topic
+	pub_again.publish(ranges.header,minpoint,maxpoint)
 
 def listener():
 	rospy.init_node('listenlaserscan',anonymous=True)
