cb99003603c73e303c185facef12ad94b8495c25
Manavendra Desai <manavendradesai@wayne.edu>
2022-06-21 16:53:55 -0400

    Rosbag, custom launch files and rviz files created. Lab1 complete!

--- lab1/scripts/listenlaserscan.py
+++ lab1/scripts/listenlaserscan.py
@@ -12,12 +12,16 @@ minusnone = -1;
 def callback(ranges):
 	#Filter NaNs and Inf
 	mapping = {np.nan:0,np.inf:0} 
-	filteredranges = [mapping.get(x,x) for x in ranges.ranges]	
+	filteredranges = [mapping.get(x,x) for x in ranges.ranges]
+	ranges.ranges = filteredranges	
 	
 	#Publish max min range over two separate topics
 	pub_close = rospy.Publisher('closest_point',Float64,queue_size=1)
 	pub_far = rospy.Publisher('farthest_point',Float64,queue_size=1)
-	pub_again = rospy.Publisher('scan_range',scan_range,queue_size=1)
+	pub_custom = rospy.Publisher('scan_range',scan_range,queue_size=1)
+
+	#Publish filtered laserscan over a separate topic
+	pub_filt = rospy.Publisher('scan_filt',LaserScan,queue_size=1)
 
 	minpoint = sorted(filteredranges)[0] if minusnone not in filteredranges else sorted(filteredranges)[1] 
 	maxpoint = max(filteredranges)
@@ -26,8 +30,9 @@ def callback(ranges):
 	pub_close.publish(minpoint)
 	pub_far.publish(maxpoint)
 
-	#Publish laserscan header, min and maxpoint through a message on another topic
-	pub_again.publish(ranges.header,minpoint,maxpoint)
+	#Publish laserscan header, min and maxpoint through a custom message on another topic
+	pub_custom.publish(ranges.header,minpoint,maxpoint)
+	pub_filt.publish(ranges)
 
 def listener():
 	rospy.init_node('listenlaserscan',anonymous=True)
